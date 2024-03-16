17dc20f33904c4d13bf290e1611a014ef0127e83
ananthvk <123456789987654321sr@gmail.com>
2024-03-16 20:34:49 +0530

    Switched to meson

---
+++ tests/test_singlylist.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+
+TEST(BasicTest, FirstOne)
+{
+    ASSERT_EQ(0, 0);
+}
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
